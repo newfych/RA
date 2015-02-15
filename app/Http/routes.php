4b0705adefbeadecd1c915e2f40ff8e1c5ca304a
newfych <newfych@yahoo.com>
2015-02-15 23:38:06 +0500

    Add route for device resource and view templates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::resource('devices', 'DevicesController');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
