df8d87a91e73a2dfcc9d2ea19b1ba1833b4ac961
newfych <newfych@yahoo.com>
2015-02-16 02:40:18 +0500

    <div class= "form-horizontal"> added to create device form

--- resources/views/devices/create.blade.php
+++ resources/views/devices/create.blade.php
@@ -2,27 +2,27 @@
 
 @section('content')
     <div class="col-md-4 col-md-offset-4"><h2>Create new device</h2></div>
+    <div class="form-horizontal">
+        {!! Form::open(['url' => 'devices']) !!}
 
-    {!! Form::open(['url' => 'devices']) !!}
-
-        <div class="form-group">
-            <div class="col-md-4 col-md-offset-4">
-                {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Device name']) !!}
+            <div class="form-group">
+                <div class="col-md-4 col-md-offset-4">
+                    {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Device name']) !!}
+                </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <div class="col-md-4 col-md-offset-4">
-                {!! Form::text('imageName', null, ['class'=>'form-control', 'placeholder'=>'Image name']) !!}
+            <div class="form-group">
+                <div class="col-md-4 col-md-offset-4">
+                    {!! Form::text('imageName', null, ['class'=>'form-control', 'placeholder'=>'Image name']) !!}
+                </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <div class="col-md-2 col-md-offset-5">
-                {!! Form::submit('Create', ['class'=>'btn btn-primary form-control']) !!}
+            <div class="form-group">
+                <div class="col-md-2 col-md-offset-5">
+                    {!! Form::submit('Create', ['class'=>'btn btn-primary form-control']) !!}
+                </div>
             </div>
-        </div>
 
-    {!! Form::close() !!}
-
+        {!! Form::close() !!}
+    </div>
 @endsection
